7350fd7767715801f636bb3af493a3d03921fa75
HelloEliin <98847181+HelloEliin@users.noreply.github.com>
2022-09-09 10:44:17 +0200

    Add files via upload
    Jag har en mapp där de uppladdade bilderna hamnar i ;P

---
+++ web/1b/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+ Select image to upload:<br><br>
+  <input type="file" name="file">
+  <button type="submit" name="submit"> Upload </button>
+</form>
+
+</body>
+</html>
